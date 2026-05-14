d65d8f9cc7ce3788e037328bdbc982d75c477b59
angie <angiearauz70@gmail.com>
2026-05-14 11:42:46 -0600

    eliminanción de archivos que no se usan

--- app/src/main/java/com/example/dataclassactivitiesapp/MainActivity.kt
+++ app/src/main/java/com/example/dataclassactivitiesapp/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Surface {
-                    // AppNavigation será creado en la Fase 2
                     AppNavigation()
                 }
             }
